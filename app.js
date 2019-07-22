a50d2d43bd3d714081da2a23abadf5a66ddba3fd
Liam Dawson <liamrdawson@gmail.com>
2019-07-22 23:31:28 +0100

    introduce the problem

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+/*
+    The problem we have is that there are two function which are each updating the same global variable.
+    What can we do about this wthout creating a new variable to keep track of each time a function is created with the same type of output?
+*/
+
+
 var count = 0;
 
 function countBirds() {
   count += 1;
   return count + ' birds';
 }
+
+function countDogs() {
+    count += 1;
+    return count + ' dogs';
+}
+
